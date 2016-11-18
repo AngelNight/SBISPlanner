d923859d62abb4dbfe2c0b1d617deb094537e186
deonisnov <deonis.novikov@gmail.com>
2016-11-18 21:45:54 +0300

    Added XHR wrapper. getTasks() refactoring

--- background.js
+++ background.js
@@ -17,10 +17,43 @@ chrome.runtime.onMessage.addListener(function(utterance,sender,callback) {
 
 });
 
+function getDomain(){
+    return 'https://'+window.location.hostname+'/';    
+}
+
+
+// XHR wrapper to do some request
+function request(method,url,body,headers,callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open(method,url,true);
+    // Use browser cookie's
+    xhr.withCredentials = true;
+
+    if(Array.isArray(headers)) headers.forEach( function(item){
+        xhr.setRequestHeader(item.key,item.value);
+    });
+
+    xhr.send(JSON.stringify(body));
+
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) callback(true,'Request error.Ready state:'+x.readyState);
+    if (xhr.status != 200) {
+        callback(true,'Server response code:'+xhr.status+'\nResponse body'+xhr.responseText);
+        console.error(xhr.responseText);
+    } else {
+        var response = JSON.parse(xhr.responseText);
+        callback(false,response);
+    }
+
+    }
+}
+
+
+
 // Function to get task list
+
 function getTasks(){
 var url = getDomain()+'service/';
-var xhr = new XMLHttpRequest();
 var body = {"jsonrpc":"2.0","protocol":4,
             "method":"СвязьПапок.ДокументыВПапке",
             "params":{"Фильтр":{"s":[{"n":"ВключитьСписокИсполнителей","t":"Логическое"},
@@ -32,23 +65,17 @@ var body = {"jsonrpc":"2.0","protocol":4,
             {"n":"РазмерСтраницы","t":"Число целое"},{"n":"Страница","t":"Число целое"}],
             "d":[true,25,0],"_type":"record"},"ДопПоля":[]},"id":1};
 
-xhr.open('POST',url,true);
-xhr.withCredentials = true;
-xhr.setRequestHeader('x-calledmethod','SvyazPapok.DokumentyVPapke');
-xhr.setRequestHeader('x-originalmethodname','0KHQstGP0LfRjNCf0LDQv9C+0Lou0JTQvtC60YPQvNC10L3RgtGL0JLQn9Cw0L/QutC1');
-xhr.setRequestHeader('content-type','application/json; charset=utf-8');
+var headers = [
+    {key:'x-calledmethod',value:'SvyazPapok.DokumentyVPapke'},
+    {key:'x-originalmethodname',value:'0KHQstGP0LfRjNCf0LDQv9C+0Lou0JTQvtC60YPQvNC10L3RgtGL0JLQn9Cw0L/QutC1'},
+    {key:'content-type',value:'application/json; charset=utf-8'}
+];
 
-xhr.send(JSON.stringify(body));
-
-xhr.onreadystatechange = function() {
-  if (xhr.readyState != 4) return;
-  console.log("Cтатус"+xhr.status);
-  if (xhr.status != 200) {  P
-    console.error(xhr.responseText);
-  } else {
-    var body = xhr.responseText;
-    var result = body.result.d;
-    result.forEach( function(item) {console.log(item[10]); });
-  }
-
+request('POST',url,body,headers,function(err,response){
+    if(err) console.err(response);
+    console.log(response);
+    response.result.d.forEach(function(item) {
+        console.log('Приоритет %s\nТип задачи: %s \nСрок: %s',item[30],item[10],item[5])
+    });
+});
 }
